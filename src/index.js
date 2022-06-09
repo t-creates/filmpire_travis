6ed5c02d7c191adb1bfe08b6b330e1f9f089f162
Travis Geislinger <travisgeislinger@Traviss-MacBook-Pro.local>
2022-06-09 06:47:47 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// This is what connects our React App to our Public/index.html
+import ReactDOM from 'react-dom';
+// All of the componets will get passed through using the "App" and then rendered to the index.html file
+import App from './components/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Passes our react app and fetches the document (root element of our app)
+ReactDOM.render(<App />, document.getElementById('root'));
