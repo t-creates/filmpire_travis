151ee9486076111925303905b6a68aeafb09de3d
Travis Geislinger <travisgeislinger@Traviss-MacBook-Pro.local>
2022-06-15 12:02:16 -0600

    Adding components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 // This is what connects our React App to our Public/index.html
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+
+// React Router is a fully-featured client and server-side routing library for React, a JavaScript library for building user interfaces.
+import { BrowserRouter } from 'react-router-dom';
+
 // All of the componets will get passed through using the "App" and then rendered to the index.html file
 import App from './components/App';
 
 // Passes our react app and fetches the document (root element of our app)
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
