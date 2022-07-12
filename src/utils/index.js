ee4702b50707658c12f228852c18ccc423efc208
t-creates <geislinger@pm.me>
2022-07-12 16:41:16 -0600

    Utils API for authenticating user profile

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,16 @@
 import axios from 'axios';
 
 const moviesApi = axios.create({
-
+  baseURL: 'https://api.themoviedb.org/3/',
+  params: {
+    api_key: process.env.REACT_APP_TMBD_API_KEY,
+  },
 });
 
 export const fetchToken = async () => {
+  try {
 
+  } catch (error) {
+
+  }
 };
